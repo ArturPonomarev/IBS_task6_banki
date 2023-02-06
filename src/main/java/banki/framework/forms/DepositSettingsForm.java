ef81d19efa97241ee06f165e7b516e8ce9fddfbd
Artur <volshebnik40.ru@gmail.com>
2023-02-06 21:44:54 +0300

    Add bank selector, add options selector

--- src/main/java/banki/framework/forms/DepositSettingsForm.java
+++ src/main/java/banki/framework/forms/DepositSettingsForm.java
@@ -1,6 +1,7 @@
 package banki.framework.forms;
 
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,6 +10,11 @@ import java.util.List;
 import java.util.Optional;
 
 public class DepositSettingsForm extends BaseForm {
+
+    String clickOptionCheckboxByTextTemplate = "//*[contains(@class,'SearchModal')]//*[contains(text(),'%s')]/../*[@data-testid='checkbox-icon-wrapper']";
+
+    @FindBy(xpath = "//*[contains(@class,'SearchModal__StyledBody')]")
+    WebElement uniqueElement;
 
     @FindBy(xpath = "//*[contains(@class,'SearchModal')]//*[contains(text(),'Тип вклада')]/..//*[@role='button']")
     WebElement depositTypeListOpener;
@@ -19,11 +25,18 @@ public class DepositSettingsForm extends BaseForm {
     @FindBy(xpath = "//*[contains(@class,'SearchModal')]//input[@name='amount']")
     WebElement depositAmountField;
 
-    @FindBy(xpath = "//*[contains(@class,'SearchModal__StyledBody')]")
-    WebElement uniqueElement;
+    @FindBy(xpath = "//*[text()='Банки']/../input")
+    WebElement banksListOpener;
+
+    @FindBy(xpath = "//*[@data-placement = 'bottom']")
+    WebElement anyOpenedList;
 
     @FindBy(xpath = "//*[contains(@class,'SearchModal')]//*[@data-placement]//li//*[text()]")
     List<WebElement> dropdownElements;
+
+    @FindBy(xpath = "//*[@checked]/..//*[text()]")
+    List<WebElement> selectedBanks;
+
 
     @Override
     public boolean isPageOpen() {
@@ -54,6 +67,51 @@ public class DepositSettingsForm extends BaseForm {
                 () -> Assertions.fail("В списке 'Тип вклада' не найден элемент " + type));
     }
 
+    public void openBanksList() {
+        banksListOpener.click();
+
+        try {
+            wait.until(dr-> anyOpenedList.isDisplayed());
+        } catch (TimeoutException e) {
+            Assertions.fail("Список банков не открыт");
+        }
+    }
+
+    public void closeBanksList() {
+        banksListOpener.click();
+    }
+
+    public void selectBanks(List<String> bankNames) {
+        for (var bankName : bankNames) {
+            var bankCheckbox = findInDropdown(bankName);
+
+            bankCheckbox.ifPresentOrElse(
+                    WebElement::click,
+                    () -> Assertions.fail("В списке 'Банки' не найден элемент " + bankName));
+
+            if (!isBankSelected(bankName))
+                Assertions.fail("Банк: " + bankName + " отсутствует в списке выбранных банкой");
+        }
+    }
+
+    public void selectAdvanceOption(String optionName) {
+        try {
+            wait.until(dr -> dr.findElement(By.xpath(String.format(clickOptionCheckboxByTextTemplate,optionName)))).click();
+        } catch (TimeoutException e) {
+            Assertions.fail("В списке опций отсутствует элемент " + optionName);
+        }
+    }
+
+    private boolean isBankSelected(String bankName) {
+        try {
+            wait.until(dr -> selectedBanks.stream().anyMatch(element -> element.getText().equals(bankName)));
+            System.out.println("depositSettings,isbankSelected: Найден банк " + bankName);
+            return true;
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
     private Optional<WebElement> findInDropdown(String target) {
         Optional<WebElement> el = Optional.empty();
         try {
@@ -64,6 +122,7 @@ public class DepositSettingsForm extends BaseForm {
                     .filter(element -> element.getText().equals(target))
                     .findFirst();
         } catch (TimeoutException ignore) {
+            //* Нам не нужно обрабатывать ошибку т.к. по факту она является лишь маркером того что элемент не найден и Optional будет с null'ом
         }
         return el;
     }
--- src/test/java/banki/tests/CommonDepositTest.java
+++ src/test/java/banki/tests/CommonDepositTest.java
@@ -6,10 +6,14 @@ import banki.framework.forms.MainPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class CommonDepositTest extends BaseTest{
 
     //TODO: в тестовые данные
     private final String DEPOSIT_MENU_NAME = "Вклады";
+
+    private final List<String> bankNamesList = List.of(new String[]{"Банк «Открытие»","Тинькофф Банк","ВТБ","Газпромбанк","Сбербанк"}); //TODO: test data
 
     private MainPage mainPage = new MainPage();
     private DepositPage depositPage = new DepositPage();
@@ -26,5 +30,11 @@ public class CommonDepositTest extends BaseTest{
         //TODO:Проверка значения в поле
         depositSetting.selectDepositTerm("6 месяцев");//TODO: test data
         depositSetting.selectDepositType("Обычные вклады");//TODO: test data
+        depositSetting.openBanksList();
+        depositSetting.selectBanks(bankNamesList);
+        depositSetting.closeBanksList();
+        depositSetting.selectAdvanceOption("Со снятием");//TODO: test data
+        depositSetting.selectAdvanceOption("С пополнением");//TODO: test data
+        depositSetting.selectAdvanceOption("С капитализацией");//TODO: test data
     }
 }
